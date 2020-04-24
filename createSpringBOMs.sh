47fbe3016c3f1af1fb7fff5bc19b8b64bd56b64b
Michael Gissing <michael@gissing.at>
2020-04-24 11:43:17 +0200

    add CycloneDX boms for Spring 2.2.6, add first SPDX boms, add dependency check reports

--- createSpringBOMs.sh
+++ createSpringBOMs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-. functions.sh
+. ./functions.sh
 
 inf "starting '${0}'"
 
 set -e
 set -x
+
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
 
 for i in ./dependency-demo-spring-boot-*; do
   pushd "${i}"
@@ -17,6 +19,10 @@ for i in ./dependency-demo-spring-boot-*; do
   ./mvnw verify
 
   cp target/bom.xml "../boms/${NAME}.bom.xml"
+
+  cp target/site/*.spdx "../boms/${NAME}.spdx" || true
+
+  cp target/dependency-check-report.html "../reports/dependency-check-report.${NAME}.${TIMESTAMP}.html" || true
 
   popd
 done
