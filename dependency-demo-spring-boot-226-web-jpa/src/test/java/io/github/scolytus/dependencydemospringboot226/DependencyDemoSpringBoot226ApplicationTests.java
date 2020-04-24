2dbcff73bf4b8632cf80905d170cc96ef2fdced7
Michael Gissing <michael@gissing.at>
2020-04-24 10:59:44 +0200

    add OOTB dependency-demo-spring-boot-226 and dependency-demo-spring-boot-226-web-jpa

---
+++ dependency-demo-spring-boot-226-web-jpa/src/test/java/io/github/scolytus/dependencydemospringboot226/DependencyDemoSpringBoot226ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.scolytus.dependencydemospringboot226;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyDemoSpringBoot226ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ dependency-demo-spring-boot-226/src/main/java/io/github/scolytus/dependencydemospringboot226/DependencyDemoSpringBoot226Application.java
@@ -0,0 +1,13 @@
+package io.github.scolytus.dependencydemospringboot226;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyDemoSpringBoot226Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyDemoSpringBoot226Application.class, args);
+	}
+
+}
